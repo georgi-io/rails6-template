d68b295e3fcae11fb95fb17ae04d107c43d72a59
Sebastian Georgi <sebastian@georgi.io>
2020-12-10 23:22:24 +0100

    Added Bootstrap/Material to Webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,28 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "controllers"
+
+import $ from 'jquery';
+import 'bootstrap/dist/js/bootstrap';
+import 'daemonite-material/js/material';
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import "controllers"
+$(document).on('turbolinks:load', function () {
+  $('body').tooltip({
+    selector: '[data-toggle="tooltip"]',
+    container: 'body',
+  }).popover({
+    selector: '[data-toggle="popover"]',
+    container: 'body',
+    html: true,
+    trigger: 'hover',
+  });
+
+  $(
+    '.floating-label .custom-select, .floating-label .form-control',
+  ).floatinglabel();
+});
