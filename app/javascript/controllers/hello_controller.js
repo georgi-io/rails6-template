8a9c0c1332b9f1680db97f734be28a79da7c11f2
Sebastian Georgi <sebastian@georgi.io>
2020-12-29 14:35:58 +0100

    Further Stimulus style class operations

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -10,10 +10,11 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = ['output', 'button']
+  static targets = ['output', 'button', 'container']
 
   greet() {
     this.outputTarget.textContent = 'Hello, this is a Rails 6.1 template with StimulusJS!';
+    this.containerTarget.classList.replace('alert-primary', 'alert-info');
     this.buttonTarget.hidden = true;
   }
 }
